916f2a5335226d84bd50c45b67cba84efda41635
Simon Zweers <simon.zweers@hva.nl>
2024-03-16 12:20:22 +0100

    reformatt

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -29,17 +29,17 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 require('luasnip.loaders.from_vscode').lazy_load()
 
 cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'nvim_lua'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-  },
-  formatting = lsp_zero.cmp_format(),
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<CR>'] = cmp.mapping.confirm(),
-  }),
+	sources = {
+		{name = 'path'},
+		{name = 'nvim_lsp'},
+		{name = 'nvim_lua'},
+		{name = 'luasnip', keyword_length = 2},
+		{name = 'buffer', keyword_length = 3},
+	},
+	formatting = lsp_zero.cmp_format(),
+	mapping = cmp.mapping.preset.insert({
+		['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+		['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+		['<CR>'] = cmp.mapping.confirm(),
+	}),
 })
