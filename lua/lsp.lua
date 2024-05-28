0baa29420b7225e148f3bb72971426b036dfbf36
Simon Zweers <s.zweers21@gmail.com>
2024-05-28 22:23:52 +0200

    add keybind for function signature help

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -30,7 +30,7 @@ lsp_zero.on_attach(function(client, bufnr)
 	local opts = {buffer = bufnr, remap = false}
 
 	vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "<leader>h", function() vim.lsp.buf.signature_help() end, opts)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 lsp_zero.setup()
