46f697248ee92aa4b6d2ec6b41c138d81e1206ea
Simon Zweers <s.zweers21@gmail.com>
2024-06-25 23:54:56 +0200

    Fixed lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,10 @@
 return {
 	{
 		'nvim-lualine/lualine.nvim',
-		dependencies = { 'nvim-tree/nvim-web-devicons' }
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
+		config = function()
+			require('lualine').setup()
+		end
 	}
 }
 
