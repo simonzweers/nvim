9e0f87a357d0fc15941747fd95fe6393aedc051c
Simon Zweers <s.zweers21@gmail.com>
2024-07-06 20:21:45 +0200

    Added theme echo shortcut

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -14,17 +14,22 @@ local themes = {
 	"vscode",
 }
 
+local theme_index = 1
+
 function set_theme(index) 
 	if index > #themes then
 		print(string.format("No theme at index %d", #themes))
 	end
-	print(string.format("Selected theme: %s", themes[index]))
 	vim.cmd(string.format('colorscheme %s', themes[index]))
 end
 
 function random_theme()
-	index = math.random(#themes)
-	set_theme(index)
+	theme_index = math.random(#themes)
+	set_theme(theme_index)
+end
+
+function what_theme()
+	print(string.format("Selected theme: %s", themes[theme_index]))
 end
 
 vim.keymap.set('n', '<leader>hihi', random_theme, {})
@@ -38,6 +43,7 @@ vim.keymap.set('n', '<leader>th6', function() set_theme(6) end, {})
 vim.keymap.set('n', '<leader>th7', function() set_theme(7) end, {})
 vim.keymap.set('n', '<leader>th8', function() set_theme(8) end, {})
 vim.keymap.set('n', '<leader>th9', function() set_theme(9) end, {})
+vim.keymap.set('n', '<leader>th?', function() what_theme() end, {})
 
 set_theme(1)
 
