7af26891ed26e2513cb038d0b27e68c0862b9b0c
Simon Zweers <simon.zweers@hva.nl>
2025-02-18 14:09:17 +0100

    Added conform for formatting

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+		config = function()
+			local conform = require("conform")
+			conform.setup({
+				formatters_by_ft = {
+					cpp = { "clang_format" }
+				},
+				formatters = {
+					clang_format = {
+						prepend_args = {"--style=file:.clang-format"}
+					}
+				},
+			})
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				pattern = "*",
+				callback = function(args)
+					require("conform").format({ bufnr = args.buf })
+				end,
+			})
+		end
+	}
+}
