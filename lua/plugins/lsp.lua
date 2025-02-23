63d105602c57ba71c6f37b00e64b6e4a783373bd
Simon Zweers <simon.zweers@hva.nl>
2025-02-23 19:29:06 +0100

    add file to lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -89,29 +89,52 @@ return {
 
 		end
 	},
-	{'hrsh7th/cmp-nvim-lsp'},
 	{
 		'hrsh7th/nvim-cmp',
 		config = function() 
 			local cmp = require('cmp')
 			local cmp_select = {behavior = cmp.SelectBehavior.Select}
 			require('luasnip.loaders.from_vscode').lazy_load()
+			local luasnip = require("luasnip")
 			cmp.setup({
-				sources = {
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				sources = cmp.config.sources({
+					{name = 'luasnip', priority = 100},
+					{name = 'nvim_lsp', priority = 50},
 					{name = 'nvim_lsp_signature_help' },
 					{name = 'path'},
-					{name = 'nvim_lsp'},
 					{name = 'nvim_lua'},
-					{name = 'luasnip', keyword_length = 2},
 					{name = 'buffer', keyword_length = 3},
-				},
+				}),
 				mapping = cmp.mapping.preset.insert({
 					['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 					['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 					['<CR>'] = cmp.mapping.confirm({select = true}),
+					['<Tab>'] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { 'i', 's' }), -- Tab to expand or jump
+					['<S-Tab>'] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { 'i', 's' }), -- Shift-Tab to jump backward
 				}),
 			})
 		end
 	},
-	{'L3MON4D3/LuaSnip'},
+	{
+		'L3MON4D3/LuaSnip',
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
+	{'hrsh7th/cmp-nvim-lsp'},
 }
