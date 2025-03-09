bea2ec74ffd27d288e0d3be0f7a0263af566c1e7
Simon Zweers <simon.zweers@hva.nl>
2025-03-09 16:58:33 +0100

    Separated completion stuff into separate file

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,62 @@
+return {
+	{
+		'hrsh7th/nvim-cmp',
+		config = function() 
+			local cmp = require('cmp')
+			local cmp_select = {behavior = cmp.SelectBehavior.Select}
+			require('luasnip.loaders.from_vscode').lazy_load()
+			local luasnip = require("luasnip")
+
+			vim.keymap.set({"i"}, "<C-L>", function() luasnip.expand() end, {silent = true})
+			vim.keymap.set({"i", "s"}, "<C-K>", function() luasnip.jump( 1) end, {silent = true})
+			vim.keymap.set({"i", "s"}, "<C-J>", function() luasnip.jump(-1) end, {silent = true})
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					-- documentation = cmp.config.window.bordered(),
+				},
+				sources = cmp.config.sources({
+					{name = 'nvim_lsp', priority = 50},
+					{name = 'luasnip', priority = 100},
+					{name = 'nvim_lsp_signature_help' },
+					{name = 'path'},
+					{name = 'nvim_lua'},
+					{name = 'buffer', keyword_length = 3},
+				}),
+				mapping = cmp.mapping.preset.insert({
+					['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+					['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+					['<CR>'] = cmp.mapping.confirm({select = true}),
+					['<Tab>'] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, { 'i', 's' }), -- Tab to expand or jump
+					['<S-Tab>'] = cmp.mapping(function(fallback)
+						if luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { 'i', 's' }), -- Shift-Tab to jump backward
+				}),
+			})
+		end
+	},
+	{
+		'L3MON4D3/LuaSnip',
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"saadparwaiz1/cmp_luasnip"
+		},
+	},
+	{'hrsh7th/cmp-nvim-lsp'},
+}
