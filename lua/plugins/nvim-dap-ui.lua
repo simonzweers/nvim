1c9a009068620578e6dcd91ec6acaa21fd026200
Simon Zweers <simon.zweers@protonmail.com>
2025-03-23 22:56:46 +0100

    dapui stil notworkign for c--

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -1,39 +1,32 @@
 return {
 	{
 		"rcarriga/nvim-dap-ui",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio",
-			"theHamsta/nvim-dap-virtual-text"
+		dependencies = { "nvim-neotest/nvim-nio" },
+		-- stylua: ignore
+		keys = {
+			{ "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+			{ "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
 		},
-		config = function ()
+		opts = {},
+		config = function(_, opts)
+			local dap = require("dap")
 			local dapui = require("dapui")
-			dapui.setup()
-			local dap = require("dap")
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
+			dapui.setup(opts)
+			dap.listeners.after.event_initialized["dapui_config"] = function()
+				dapui.open({})
 			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
+			dap.listeners.before.event_terminated["dapui_config"] = function()
+				dapui.close({})
 			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
+			dap.listeners.before.event_exited["dapui_config"] = function()
+				dapui.close({})
 			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
-		end,
-	},
-	{
-		"mfussenegger/nvim-dap",
-		config = function ()
-			local dap = require("dap")
 			dap.adapters.gdb = {
 				type = "executable",
 				command = "gdb",
 				args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
 			}
-			dap.configurations.c = {
+			dap.configurations.cpp = {
 				{
 					name = "Launch",
 					type = "gdb",
@@ -68,30 +61,44 @@ return {
 					cwd = '${workspaceFolder}'
 				},
 			}
-			dap.configurations.cpp = dap.configurations.c
-			-- local wk = require("which-key")
-			-- wk.add({
-			-- 	{
-			-- 		mode = 'n',
-			-- 		{ "<leader>dB", function() dap.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
-			-- 		{ "<leader>db", function() dap.toggle_breakpoint() end, desc = "Toggle Breakpoint" },
-			-- 		{ "<leader>dc", function() dap.continue() end, desc = "Run/Continue" },
-			-- 		{ "<leader>da", function() dap.continue({ before = get_args }) end, desc = "Run with Args" },
-			-- 		{ "<leader>dC", function() dap.run_to_cursor() end, desc = "Run to Cursor" },
-			-- 		{ "<leader>dg", function() dap.goto_() end, desc = "Go to Line (No Execute)" },
-			-- 		{ "<leader>di", function() dap.step_into() end, desc = "Step Into" },
-			-- 		{ "<leader>dj", function() dap.down() end, desc = "Down" },
-			-- 		{ "<leader>dk", function() dap.up() end, desc = "Up" },
-			-- 		{ "<leader>dl", function() dap.run_last() end, desc = "Run Last" },
-			-- 		{ "<leader>do", function() dap.step_out() end, desc = "Step Out" },
-			-- 		{ "<leader>dO", function() dap.step_over() end, desc = "Step Over" },
-			-- 		{ "<leader>dP", function() dap.pause() end, desc = "Pause" },
-			-- 		{ "<leader>dr", function() dap.repl.toggle() end, desc = "Toggle REPL" },
-			-- 		{ "<leader>ds", function() dap.session() end, desc = "Session" },
-			-- 		{ "<leader>dt", function() dap.terminate() end, desc = "Terminate" },
-			-- 		{ "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
-			-- 	},
-			-- })
-		end
+			dap.configurations.c = dap.configurations.cpp
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		recommended = true,
+		desc = "Debugging support. Requires language specific adapters to be configured. (see lang extras)",
+
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			-- virtual text for the debugger
+			{
+				"theHamsta/nvim-dap-virtual-text",
+				opts = {},
+			},
+		},
+		-- stylua: ignore
+		keys = {
+			{ "<leader>dB", function() require("dap").set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = "Breakpoint Condition" },
+			{ "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "Toggle Breakpoint" },
+			{ "<leader>dc", function() require("dap").continue() end, desc = "Run/Continue" },
+			{ "<leader>da", function() require("dap").continue({ before = get_args }) end, desc = "Run with Args" },
+			{ "<leader>dC", function() require("dap").run_to_cursor() end, desc = "Run to Cursor" },
+			{ "<leader>dg", function() require("dap").goto_() end, desc = "Go to Line (No Execute)" },
+			{ "<leader>di", function() require("dap").step_into() end, desc = "Step Into" },
+			{ "<leader>dj", function() require("dap").down() end, desc = "Down" },
+			{ "<leader>dk", function() require("dap").up() end, desc = "Up" },
+			{ "<leader>dl", function() require("dap").run_last() end, desc = "Run Last" },
+			{ "<leader>do", function() require("dap").step_out() end, desc = "Step Out" },
+			{ "<leader>dO", function() require("dap").step_over() end, desc = "Step Over" },
+			{ "<leader>dP", function() require("dap").pause() end, desc = "Pause" },
+			{ "<leader>dr", function() require("dap").repl.toggle() end, desc = "Toggle REPL" },
+			{ "<leader>ds", function() require("dap").session() end, desc = "Session" },
+			{ "<leader>dt", function() require("dap").terminate() end, desc = "Terminate" },
+			{ "<leader>dw", function() require("dap.ui.widgets").hover() end, desc = "Widgets" },
+		},
+
+		-- config = function()
+		-- end,
 	}
 }
