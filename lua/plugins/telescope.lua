c27ab9087b1a73813c5dea561bce5f1bca3cea9e
Simon Zweers <simon.zweers@protonmail.com>
2025-04-02 20:36:51 +0200

    Comment out treesitter keybinds defined by snacks.nvim

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,15 +6,15 @@ return {
 		config = function()
 			local builtin = require('telescope.builtin')
 			
-			local wk = require("which-key")
-			wk.add({
-				{'<leader>ff', builtin.find_files, desc = "Find Files", mode = 'n'},
-				{'<leader>fr', builtin.live_grep, desc = "Live Grep", mode = 'n'},
-				{'<leader>fg', builtin.git_files, desc = "Find Git Files", mode = 'n'},
-				{'<leader>fh', builtin.help_tags, desc = "Help Tags", mode = 'n'},
-				{'<leader>gr', builtin.lsp_references, desc = "Go to LSP reference", mode = 'n'},
-				{'<leader>fb', builtin.buffers, desc = "Find buffer", mode = 'n'},
-			})
+			-- local wk = require("which-key")
+			-- wk.add({
+			-- 	-- {'<leader>ff', builtin.find_files, desc = "Find Files", mode = 'n'},
+			-- 	-- {'<leader>fr', builtin.live_grep, desc = "Live Grep", mode = 'n'},
+			-- 	-- {'<leader>fg', builtin.git_files, desc = "Find Git Files", mode = 'n'},
+			-- 	-- {'<leader>fh', builtin.help_tags, desc = "Help Tags", mode = 'n'},
+			-- 	-- {'<leader>gr', builtin.lsp_references, desc = "Go to LSP reference", mode = 'n'},
+			-- 	-- {'<leader>fb', builtin.buffers, desc = "Find buffer", mode = 'n'},
+			-- })
 		end
 	}
 }
