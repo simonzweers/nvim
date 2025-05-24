1f0f22a58868e9e424d0671baf1e72e780a0c7fd
Simon Zweers <simon.zweers@protonmail.com>
2025-05-24 13:06:20 +0200

    Remove indentation level in mini.vim

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,25 +1,23 @@
 return {
 	{
-		{
-			"echasnovski/mini.nvim",
-			version = "*",
-			config = function()
-				local hipatterns = require("mini.hipatterns")
-				hipatterns.setup({
-					highlighters = {
-						-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-						fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-						hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-						todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-						note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+		"echasnovski/mini.nvim",
+		version = "*",
+		config = function()
+			local hipatterns = require("mini.hipatterns")
+			hipatterns.setup({
+				highlighters = {
+					-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
 
-						-- Highlight hex color strings (`#rrggbb`) using that color
-						hex_color = hipatterns.gen_highlighter.hex_color(),
-					},
-				})
+					-- Highlight hex color strings (`#rrggbb`) using that color
+					hex_color = hipatterns.gen_highlighter.hex_color(),
+				},
+			})
 
-				local surround = require("mini.surround").setup()
-			end,
-		},
+			local surround = require("mini.surround").setup()
+		end,
 	},
 }
