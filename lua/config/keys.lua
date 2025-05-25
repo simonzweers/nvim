6cb8e913c5161ca2cfd113cd20ea1ac908579629
Simon Zweers <simon.zweers@protonmail.com>
2025-05-25 16:48:25 +0200

    Added more shortcuts

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -3,5 +3,7 @@ vim.keymap.set("n", "<C-s>", vim.cmd.wa, { desc = "Save file" })
 vim.keymap.set("n", "<leader>qq", vim.cmd.wqa, { desc = "Save and Quit" })
 vim.keymap.set("v", "<C-c>", '"+y<CR>', { desc = "Copy into OS buffer" })
 
--- Insert newline under cursor
+-- Insert newline under/above cursor
 vim.keymap.set("n", "<CR>", "o<Esc>", { desc = "Insert line below cursor" })
+vim.keymap.set("n", "<leader>o", "o<Esc>", { desc = "Insert line below cursor" })
+vim.keymap.set("n", "<leader>O", "O<Esc>", { desc = "Insert line above cursor" })
