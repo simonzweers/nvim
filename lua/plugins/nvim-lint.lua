4a656566facd2977fa89b9b39c9107139928ecaa
Simon Zweers <simon.zweers@protonmail.com>
2025-05-25 17:05:18 +0200

    Expanded linter config

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,14 +1,29 @@
 return {
 	{
 		"mfussenegger/nvim-lint",
+		-- event = "VeryLazy",
+		lazy = false,
+		opts = {
+			linters_by_ft = {
+				markdown = { "vale" },
+				bash = { "shellcheck" },
+				sh = { "shellcheck" },
+				cpp = { "cpplint" },
+			},
+		},
 		config = function()
 			local lint = require("lint")
-			lint.linters_by_ft = {
-				markdown = { "vale" },
-				bash = { "bash" },
-				sh = { "shfmt" },
-				cpp = { "clangtidy" },
-			}
+			vim.api.nvim_create_autocmd({ "TextChanged" }, {
+				callback = function()
+					-- try_lint without arguments runs the linters defined in `linters_by_ft`
+					-- for the current filetype
+					require("lint").try_lint()
+
+					-- -- You can call `try_lint` with a linter name or a list of names to always
+					-- -- run specific linters, independent of the `linters_by_ft` configuration
+					-- require("lint").try_lint("cspell")
+				end,
+			})
 		end,
 	},
 }
