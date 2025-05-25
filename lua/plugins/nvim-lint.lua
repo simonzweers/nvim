7e2bb4b0d4b2c8a8d5ad2b31f616138394b06b9a
Simon Zweers <simon.zweers@protonmail.com>
2025-05-25 17:05:39 +0200

    haha

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -17,7 +17,7 @@ return {
 				callback = function()
 					-- try_lint without arguments runs the linters defined in `linters_by_ft`
 					-- for the current filetype
-					require("lint").try_lint()
+					lint.try_lint()
 
 					-- -- You can call `try_lint` with a linter name or a list of names to always
 					-- -- run specific linters, independent of the `linters_by_ft` configuration
