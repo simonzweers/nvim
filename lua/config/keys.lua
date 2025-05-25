84e2bf91ebad8d522bf36b643360e21a0142c12d
Simon Zweers <simon.zweers@protonmail.com>
2025-05-25 16:46:47 +0200

    Added shortcut for insertin newline under cursor

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -2,3 +2,6 @@ vim.keymap.set("n", "<C-s>", vim.cmd.wa, { desc = "Save file" })
 
 vim.keymap.set("n", "<leader>qq", vim.cmd.wqa, { desc = "Save and Quit" })
 vim.keymap.set("v", "<C-c>", '"+y<CR>', { desc = "Copy into OS buffer" })
+
+-- Insert newline under cursor
+vim.keymap.set("n", "<CR>", "o<Esc>", { desc = "Insert line below cursor" })
