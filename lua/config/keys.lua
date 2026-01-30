94ff35f1fca27fcba3c89bf6cde2be87055a37dc
Simon Zweers <simon@simon-win-pc.home>
2026-01-30 14:18:48 +0100

    added keybind to select all

--- lua/config/keys.lua
+++ lua/config/keys.lua
@@ -2,6 +2,7 @@ vim.keymap.set("n", "<C-s>", vim.cmd.wa, { desc = "Save file" })
 
 vim.keymap.set("n", "<leader>qq", vim.cmd.wqa, { desc = "Save and Quit" })
 vim.keymap.set("v", "<C-c>", '"+y<CR>', { desc = "Copy into OS buffer" })
+vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
 
 -- Insert newline under/above cursor
 vim.keymap.set("n", "<CR>", "o<Esc>", { desc = "Insert line below cursor" })
