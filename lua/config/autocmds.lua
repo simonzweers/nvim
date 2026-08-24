ce15d150155b5218969957b038c189b1b7cbcb7f
Simon Zweers <simon.zweers@protonmail.com>
2026-08-24 22:28:17 +0200

    Added autocmd for yaml

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,3 +4,12 @@
 	--end
 --})
 
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "yaml",
+	callback = function ()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+		vim.opt_local.expandtab = true
+	end
+})
