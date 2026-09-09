3ecc94e866b2cf012f5c48869236755e27ca1134
Simon Zweers <simon.zweers@protonmail.com>
2026-09-09 18:21:18 +0200

    Update oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -10,4 +10,8 @@ return {
 	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
 	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
 	lazy = false,
+	config = function()
+		require("oil").setup()
+		vim.keymap.set("n", "=", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
+	end,
 }
