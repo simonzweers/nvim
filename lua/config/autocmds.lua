26b6ee2132921d42e60a5f3e7e1bd29cb77915b6
Simon Zweers <simon.zweers@protonmail.com>
2026-09-14 10:01:15 +0200

    Added separate file options for systemverilog filetype

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,16 @@
 --vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
-	--callback = function(ev)
-		--vim.lsp.buf.document_highlight()
-	--end
+--callback = function(ev)
+--vim.lsp.buf.document_highlight()
+--end
 --})
 
+print("Autocmds")
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "systemverilog",
+	callback = function()
+		print("Detected sv filetype")
+		vim.opt_local.expandtab = true
+		vim.opt_local.tabstop = 4 -- Set the number of spaces a tab counts for
+		vim.opt_local.shiftwidth = 4 -- Set the number of spaces for each indentation level
+	end,
+})
